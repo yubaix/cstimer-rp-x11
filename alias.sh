a1ed582e4a4b596389852700bebc99a36b80e310
yubaix <160341179+yubaix@users.noreply.github.com>
2025-06-17 20:09:17 -0500

    Added the alias sh file.
    Easily allows the user to run the program with an alias using this sh file.

---
+++ alias.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Activate venv and run script
+source path/to/venv/bin/activate
+cd /path/to/main/dot/py # excluding main.py at the end
+python3 /path/to/main/dot/py # including main.py at the end
